9126642a5e1e833dbf1c24228a4696429b12255f
Elliot Bulmer <sonelliot@gmail.com>
2012-07-09 17:46:11 +0800

    Fleshing out the 'Behaviour' class to return a result value

--- Gnomic.AI/Behaviour.cs
+++ Gnomic.AI/Behaviour.cs
@@ -5,6 +5,17 @@ namespace Gnomic.AI
 {
     public abstract class Behaviour
     {
+        public enum Result
+        {
+            Invalid,            // invalid for actor
+            Success,            // successful outcome
+            Failure,            // failure outcome
+            Running             // continue to update
+        }
+
+        public abstract Result Tick(Search search);
         public abstract int Weight { get; }
+
+        protected abstract bool IsValid(IActor actor);
     }
 }
