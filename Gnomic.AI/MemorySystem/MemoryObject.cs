2a30f645e545316f5179ee10520cdccf45b8147d
Elliot Bulmer <sonelliot@gmail.com>
2012-07-17 08:18:22 +0800

    Added the base classes for the MemorySystem

---
+++ Gnomic.AI/MemorySystem/MemoryObject.cs
@@ -0,0 +1,27 @@
+﻿// MemoryObject.cs
+//
+
+namespace Gnomic.AI
+{
+    public abstract class MemoryObject
+    {
+        private float m_confidence;
+        private float m_decayRate;
+
+        public float Confidence
+        {
+            get { return m_confidence; }
+        }
+
+        public bool Update()
+        {
+            return true;
+        }
+
+        protected MemoryObject(float confidence, float decayRate)
+        {
+            m_confidence = confidence;
+            m_decayRate = decayRate;
+        }
+    }
+}
---
+++ Gnomic.AI/MemorySystem/WorkingMemory.cs
@@ -0,0 +1,29 @@
+﻿// WorkingMemory.cs
+//
+
+using System.Collections.Generic;
+
+namespace Gnomic.AI
+{
+    public sealed class WorkingMemory
+    {
+        private List<MemoryObject> m_memory = new List<MemoryObject>();
+
+        public bool TrackPerception(PerceptObject perception)
+        {
+            // TODO: Add code here to read in a PerceptObject and attempt to
+            // match it against an existing memory. If it does match then it
+            // refreshes confidence in the memory.
+
+            return true;
+        }
+
+        public bool Update()
+        {
+            // TODO: Add code here to update existing memories to determine new
+            // confidence values for the MemoryObjects.
+
+            return true;
+        }
+    }
+}
